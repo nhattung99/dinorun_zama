8a7ba08ef1b555f377d21ee81758862d77b9e770
nhattung99 <nhattung99@users.noreply.github.com>
2025-10-28 17:36:00 +0700

    Fix: Add Buffer import for Vercel build

--- frontend-fhe-race/src/index.tsx
+++ frontend-fhe-race/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Buffer } from 'buffer';
 
 // Polyfills for Node.js modules in browser
 (window as any).global = window;
